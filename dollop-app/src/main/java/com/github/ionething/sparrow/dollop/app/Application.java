9cb043877517950630896c14687deb72d34db83c
Xin <vincent7xin@gmail.com>
2019-04-16 11:26:14 +0800

    骨架

---
+++ dollop-app/src/main/java/com/github/ionething/sparrow/dollop/app/Application.java
@@ -0,0 +1,16 @@
+package com.github.ionething.sparrow.dollop.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangxin
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
