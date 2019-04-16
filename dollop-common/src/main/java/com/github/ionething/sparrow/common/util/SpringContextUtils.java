e55c85e0bbfa4fd0b00c1155d45b95661487cfe7
Xin <vincent7xin@gmail.com>
2019-04-16 13:00:40 +0800

    文档

---
+++ dollop-common/src/main/java/com/github/ionething/sparrow/common/util/SpringContextUtils.java
@@ -0,0 +1,48 @@
+package com.github.ionething.sparrow.common.util;
+
+import lombok.Getter;
+import org.springframework.beans.BeansException;
+import org.springframework.boot.context.properties.bind.Bindable;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author wangxin
+ */
+@Component
+public class SpringContextUtils implements ApplicationContextAware {
+
+    @Getter
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext context) throws BeansException {
+        applicationContext = context;
+    }
+
+    /**
+     * 获取Spring boot配置
+     * @param name
+     * @param target
+     * @param <T>
+     * @return
+     */
+    public static <T> T getProperty(String name, Class<T> target) {
+        Environment environment = applicationContext.getEnvironment();
+        Binder binder = Binder.get(environment);
+        return binder.bind(name, Bindable.of(target)).get();
+    }
+
+    /**
+     * 获取bean
+     * @param requireType
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(Class<T> requireType) {
+        return applicationContext.getBean(requireType);
+    }
+}
